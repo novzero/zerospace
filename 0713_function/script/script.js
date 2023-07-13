63ac51a5d697fb151411fbe6a6cf12933724b675
novzero <sksmsdlcodud@naver.com>
2023-07-13 12:51:48 +0900

    07131

---
+++ 0713_function/script/script.js
@@ -0,0 +1,29 @@
+// 함수 function
+
+// 함수 선언
+// function 함수의 이름(){}
+
+// var name = prompt("이름을 입력하세요.");
+// alert(name + "님 안녕하세요.");
+
+// var s = "0";
+// document.getElementById("aaa").onclick = function(){
+//   s = s+1;
+//   document.getElementById("score").innerHTML = s;
+// };
+
+
+// var age =13;
+// var height = 162;
+// if(age >= 15 && height>=150){
+//   alert("입장가능");
+// }
+// else{
+//   alert("입장 불가능");
+// }
+
+var deposit = 100000;
+var monthly = 700;
+var distance = 15;
+var top = true;
+var direction = "south";
